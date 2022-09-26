fd6bb747866a364751e483077cde71d46cfc07ea
Aboorva1 <abookannan567@gmail.com>
2022-09-26 16:04:57 +0530

    First publish

---
+++ app/models/contact.rb
@@ -0,0 +1,9 @@
+class Contact < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :phone_number, presence: true, length: {is: 10}
+    validates :email, presence: true
+    validates :company_name, presence: true
+    validates :birth_date, presence: true
+    validates :address, presence: true
+end
